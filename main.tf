81932c2c379f7f5990b7148a07727afadab38350
bikramdevops-stack <singhbikramjeet5@gmail.com>
2026-07-15 00:18:09 +0530

    rg-added

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name = "rg_admin"
+    storage_account_name = "statemanage"
+    container_name = "statemanage"
+    key_id="bikram.tfstate"
+    
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -11,4 +18,7 @@ provider "azurerm" {
   features {}
 }
 
-
+resource "azurerm_resource_group" "rg" {
+  name = "rg-bik"
+  location = "east us"
+}
